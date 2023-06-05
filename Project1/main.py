5a9ff399759792ca45132774a6aaa980d30f40d5
Rune Sehested <runesehested.rs@gmail.com>
2023-06-05 16:06:16 +0200

    Early stopping and learning rate scheduler

--- Project1/main.py
+++ Project1/main.py
@@ -1,5 +1,5 @@
-import os
 import json
+import os
 
 import CNN
 import matplotlib.pyplot as plt
@@ -8,6 +8,8 @@ import torch
 import torch.nn.functional as F
 import torchvision.utils as utils
 from data import get_dataloaders
+from EarlyStopping import EarlyStopper
+from torch.optim.lr_scheduler import ExponentialLR, ReduceLROnPlateau
 from tqdm import tqdm
 
 # Hyperparameters
@@ -22,8 +24,11 @@ num_workers = 8
 
 # Optimization/training
 optim_type = 'adam' # adam or sgd
+lrscheduler_type = 'reducelronplateau' # reducelronplateau or expdecay
+early_stopping = True
+early_stopping_patience = 10 # Only relevant for if early_stopping = True
 lr = 1e-3
-num_epochs = 1  # TODO: Implement early stopping.
+num_epochs = 100 
 
 # Paths
 save_dir = f'models/{model_type.lower()}'
@@ -45,7 +50,7 @@ print(f'Using device:\t{device}')
 print('Getting data...')
 train_dataset, val_dataset, train_loader, val_loader = get_dataloaders(batch_size, num_workers=num_workers, data_augmentation=data_augmentation)
 
-def train(model, optimizer, num_epochs=10):
+def train(model, optimizer, scheduler=None, earlystopper=None, num_epochs=10):
 
     def loss_fun(output, target):
         # NOTE: Here we assume nn.LogSoftmax output.
@@ -81,7 +86,7 @@ def train(model, optimizer, num_epochs=10):
             #Compute how many were correctly classified
             predicted = output.argmax(1)
             train_correct += (target == predicted).sum().cpu().item()
-        #Comput the val accuracy
+        #Compute the val accuracy
         val_loss = []
         val_correct = 0
         model.eval()
@@ -92,14 +97,30 @@ def train(model, optimizer, num_epochs=10):
             val_loss.append(loss_fun(output, target).cpu().item())
             predicted = output.argmax(1)
             val_correct += (target == predicted).sum().cpu().item()
+
+        mean_val_loss = np.mean(val_loss)
+
         out_dict['train_acc'].append(train_correct / len(train_dataset))
         out_dict['val_acc'].append(val_correct / len(val_dataset))
         out_dict['train_loss'].append(np.mean(train_loss))
-        out_dict['val_loss'].append(np.mean(val_loss))
+        out_dict['val_loss'].append(mean_val_loss)
         print(
-            f"Loss train: {np.mean(train_loss):.3f}\t val: {np.mean(val_loss):.3f}\t",
-            f"Accuracy train: {out_dict['train_acc'][-1]*100:.1f}%\t val: {out_dict['val_acc'][-1]*100:.1f}%"
+            f"Loss train: {np.mean(train_loss):.3f}\t val: {mean_val_loss:.3f}\t",
+            f"Accuracy train: {out_dict['train_acc'][-1]*100:.1f}%\t val: {out_dict['val_acc'][-1]*100:.1f}%",
         )
+
+        # Learning rate scheduler step
+        if scheduler is not None:
+            if type(scheduler) == ReduceLROnPlateau:
+                scheduler.step(mean_val_loss)
+            else:
+                scheduler.step()
+        
+        # Early stopping
+        if earlystopper is not None:
+            if earlystopper(mean_val_loss):
+                print('Training ended as the early stopping criteria was met.')
+                break
     return out_dict
 
 # Get model
@@ -116,10 +137,19 @@ if optim_type.lower() == 'sgd':
 elif optim_type.lower() == 'adam':
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
+# Get lr scheduler
+if lrscheduler_type == 'reducelronplateau':
+    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=3)
+elif lrscheduler_type == 'expdecay':
+    scheduler = ExponentialLR(optimizer, 2)
+
+if early_stopping:
+    earlystopper = EarlyStopper(early_stopping_patience, min_delta=0)
+
 print(f"Training CNN model type '{model_type}' using '{optim_type.upper()}' optimization.\n'{model_type}' parameters:\n\n{model}\n\n...")
 
 # main training loop
-out_dict = train(model, optimizer, num_epochs=num_epochs)
+out_dict = train(model, optimizer, scheduler=scheduler, earlystopper=earlystopper, num_epochs=num_epochs)
 
 # Save
 idx = len(os.listdir(f'{save_dir}/checkpoints'))
