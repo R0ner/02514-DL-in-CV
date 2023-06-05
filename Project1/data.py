723245c88bc9628c7a816abeff2885c75c189dfb
Rune Sehested <runesehested.rs@gmail.com>
2023-06-05 13:10:13 +0200

    data script

---
+++ Project1/data.py
@@ -0,0 +1,61 @@
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+
+train_dir = '/dtu/datasets1/02514/hotdog_nothotdog/train'
+test_dir = '/dtu/datasets1/02514/hotdog_nothotdog/test'
+
+# Standardization is done according to ImageNet (mean and std. for ImageNet)
+standardize = transforms.Normalize([0.485, 0.456, 0.406],
+                                   [0.229, 0.224, 0.225])
+standardize_inv = transforms.Compose([
+    transforms.Normalize([0, 0, 0], [1 / 0.229, 1 / 0.224, 1 / 0.225]),
+    transforms.Normalize([-0.485, -0.456, -0.406], [1, 1, 1])
+])
+
+# Make transforms
+# Shared transforms (base)
+transform_base = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((64, 64), antialias=True),  # They are not all square
+])
+
+# No augmentation
+transform = transforms.Compose([transform_base, standardize])
+
+# With augmentation
+# TODO: More types of augmentation?
+transform_augment = transforms.Compose([
+    transform_base,
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ColorJitter(.1, .1, .1, .05),
+    transforms.RandomRotation(20), standardize
+])
+
+
+def get_dataloaders(batch_size: int,
+                    num_workers: int = 8,
+                    data_augmentation: bool = True) -> tuple(DataLoader):
+    """Get train and validation dataloaders for the hotdog dataset.
+    Returns: 
+        tuple(train_loader, val_loader)    torch dataloaders for the train and validation set respectively"""
+    if data_augmentation:
+        transform_train = transform_augment
+    else:
+        transform_train = transform
+    transform_val = transform
+
+    train_dataset = datasets.ImageFolder(root=train_dir,
+                                         transform=transform_train)
+    val_dataset = datasets.ImageFolder(root=test_dir, transform=transform_val)
+
+    train_loader = DataLoader(train_dataset,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=num_workers)
+    val_loader = DataLoader(val_dataset,
+                            batch_size=batch_size,
+                            shuffle=True,
+                            num_workers=num_workers)
+    
+    return train_loader, val_loader
