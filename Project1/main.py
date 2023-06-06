b1c65b9b3608ced6690042ace743b2e8be3d36c3
JaQtae <s194264@student.dtu.dk>
2023-06-06 16:45:28 +0200

    Interactive choices for types + RN18_Freeze/RN18 added

--- Project1/main.py
+++ Project1/main.py
@@ -15,7 +15,16 @@ from tqdm import tqdm
 # Hyperparameters
 # TODO: Get hyperparameters in argparser or similar.
 # Model
-model_type = 'ResNet'
+from pick import pick
+
+title = "Which model type would you like to train?: "
+options = ["ResNet", "CNN_4", "RN18_Freeze", "RN18"]
+option = pick(options, title, indicator="-->", default_index=0)
+model_type = option[0]
+print(f"You chose: {model_type}")
+
+
+#model_type = 'ResNet' # 'resnet', 'cnn_4', 'rn18_freeze', 'rn18'
 num_res_blocks = 9 # Only relevant for ResNet.
 dropout = 0
 
@@ -24,9 +33,22 @@ batch_size = 64
 data_augmentation = True
 num_workers = 8
 
+title = "Which optimizer should be used? "
+options = ["adam", "sgd"]
+option = pick(options, title, indicator="=>", default_index=0)
+optim_type = option[0]
+print(f"You chose: {optim_type}")
+
+
 # Optimization/training
-optim_type = 'adam' # adam or sgd
-lrscheduler_type = 'reducelronplateau' # reducelronplateau or expdecay
+#optim_type = 'adam' # adam or sg
+title = "Which LR-scheduler should be used? "
+options = ["reducelronplateau", "expdecay"]
+option = pick(options, title, indicator="=>", default_index=0)
+lrscheduler_type = option[0]
+print(f"You chose: {lrscheduler_type}")
+
+#lrscheduler_type = 'reducelronplateau' # reducelronplateau or expdecay
 early_stopping = True
 early_stopping_patience = 10 # Only relevant for if early_stopping = True
 lr = 1e-3
@@ -148,6 +170,10 @@ if model_type.lower() == 'resnet':
     model = CNN.ResNet(3, 32, in_size, num_res_blocks=num_res_blocks, dropout=dropout)
 elif model_type.lower() == 'cnn_4':
     model = CNN.CNN_4(3, in_size, dropout=dropout)
+elif model_type.lower() == 'rn18_freeze':
+    model = CNN.RN18(True)
+elif model_type.lower() == 'rn18':
+    model = CNN.RN18(False)
 model.to(device)
 
 # Get optimizer
