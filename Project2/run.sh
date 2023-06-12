a6aa4ac8a775fd4210869ace5e5445da6c7c7d95
ChristianDjurhuus <christian.v.djurhuus@gmail.com>
2023-06-12 11:43:31 +0200

    Ablation study bash script

--- Project2/run.sh
+++ Project2/run.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-echo "Running models on Retina dataset"
-CUDA_VISIBLE_DEVICES=0 python main.py --model_type=CNN --data_augmentation --optimizer_type=adam --lr_scheduler=reducelronplateau --loss_function=bce_loss --batch_size=8 --data_choice=Retina --num_epochs=20
-CUDA_VISIBLE_DEVICES=0 python main.py --model_type=CNN --data_augmentation --optimizer_type=adam --lr_scheduler=reducelronplateau --loss_function=dice_loss --batch_size=8 --data_choice=Retina --num_epochs=20
+# List of models
+models=("CNN" "UNet" "UNet_base")
 
-CUDA_VISIBLE_DEVICES=0 python main.py --model_type=UNet_base --data_augmentation --optimizer_type=adam --lr_scheduler=reducelronplateau --loss_function=bce_loss --batch_size=8 --data_choice=Retina --num_epochs=20
-CUDA_VISIBLE_DEVICES=0 python main.py --model_type=UNet_base --data_augmentation --optimizer_type=adam --lr_scheduler=reducelronplateau --loss_function=dice_loss --batch_size=8 --data_choice=Retina --num_epochs=20
+# List of optimizers
+optimizers=("adam" "sgd")
 
-CUDA_VISIBLE_DEVICES=0 python main.py --model_type=UNet --data_augmentation --optimizer_type=adam --lr_scheduler=reducelronplateau --loss_function=bce_loss --batch_size=8 --data_choice=Retina --num_epochs=20
-CUDA_VISIBLE_DEVICES=0 python main.py --model_type=UNet --data_augmentation --optimizer_type=adam --lr_scheduler=reducelronplateau --loss_function=dice_loss --batch_size=8 --data_choice=Retina --num_epochs=20
+# List of loss functions
+loss_functions=("bce_loss" "dice_loss" "focal_loss" "bce_total_variation")
+
+# Set your CUDA device here
+export CUDA_VISIBLE_DEVICES=0
+
+# Loop over the models
+for model in ${models[@]}; do
+
+  # Loop over the optimizers
+  for optimizer in ${optimizers[@]}; do
+
+    # Loop over the loss functions
+    for loss_function in ${loss_functions[@]}; do
+
+      # Command to run the model with different configurations
+      python main.py \
+        --n_features 16 \
+        --model_type ${model} \
+        --data_augmentation \
+        --batch_size 2 \
+        --num_workers 8 \
+        --optimizer_type ${optimizer} \
+        --lr_scheduler reducelronplateau \
+        --early_stopping \
+        --early_stopping_patience 18 \
+        --lr 1e-3 \
+        --num_epochs 100 \
+        --loss_function ${loss_function} \
+        --data_choice Retina
+
+    done
+  done
+done
